d296debf497f4c34bf49060dbef4cdc102617f32
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-06-27 23:20:23 +0530

    Time: 2 ms (31.89%) | Memory: 47.4 MB (21.24%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,20 +1,15 @@
 class Solution {
-    int t[];
-    int dp(int[] nums, int n)
-    {
-        if(nums.length-1<=n)
-            return 0;
-        if(t[n]+1!=Integer.MAX_VALUE)
-            return t[n];
-        t[n]=Integer.MAX_VALUE -1;
-        for(int i=n+1; i<=n+nums[n]; i++)
-            t[n]=Math.min(t[n],1+dp(nums,i));
-        return t[n];
-    }
     public int jump(int[] nums) {
-        t= new int[nums.length+1];
-        for(int i=0;i<=nums.length;i++)
-            t[i]=Integer.MAX_VALUE -1;
-        return dp(nums,0);
+        int ans=0, end=0,far=0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            far= Math.max(i+nums[i],far);
+            if(i==end)
+            {
+                ans++;
+                end=far;
+            }
+        }
+        return ans;
     }
 }
