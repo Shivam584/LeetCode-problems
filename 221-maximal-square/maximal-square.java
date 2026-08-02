8316dfa838bb3cffc2122884bffd8dc545ef575f
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-08-03 02:40:31 +0530

    Time: 7 ms (95.07%) | Memory: 79.4 MB (5.84%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,19 +1,25 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
         int m=matrix.length, n=matrix[0].length;
-        int ans[][]= new int[m+1][n+1];
+        int ans[]= new int[n+1];
+        int previousDiagonal=0;
+        int res=0,temp=0;
        for(int i=1;i<=m;i++)
        {
             for(int j=1;j<=n;j++)
             {
+                temp=ans[j];
                 if(matrix[i-1][j-1]=='1')
-                    ans[i][j]=1+Math.min(Math.min(ans[i-1][j],ans[i][j-1]),ans[i-1][j-1]);
+                {
+                    ans[j]=1+Math.min(Math.min(ans[j],ans[j-1]),previousDiagonal);
+                    res=Math.max(res,ans[j]);
+                }
+                else
+                    ans[j]=0;
+                previousDiagonal=temp;
             }
        }
-       int res=0;
-       for(int i=0;i<=m;i++)
-       for(int j=0;j<=n;j++)
-        res=Math.max(res,ans[i][j]);
+    
        return res*res;
     }
 }
