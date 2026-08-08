32e84214a3196eb63f3772ea1b00b8a60dd53d3a
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-08-08 14:40:53 +0530

    Time: 15 ms (5.97%) | Memory: 49.8 MB (7.05%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,39 @@
+class Solution {
+     int t[][][];
+    int dp(int[] nums, int i,int n, int k, int h)
+    {
+        if((k==0 || i==n) && h==0)
+            return 0;
+        if((k==0 || i==n) && h==1)
+            return Integer.MIN_VALUE;
+        
+        if(t[i][k][h]!=-1)
+            return t[i][k][h];
+
+    int ans= dp(nums,i+1,n,k,h);
+        if(h==0)
+        {
+            ans=Math.max(-nums[i]+dp(nums,i,n,k,1),ans);
+        }
+        else
+        {
+            ans=Math.max(nums[i]+dp(nums,i,n,k-1,0),ans);
+        }
+        return t[i][k][h]=ans;
+    }
+    public int maxProfit(int k,int[] nums) {
+          int n=nums.length;
+          t=new int[n+1][k+1][3];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=k;j++)
+            {
+                for(int l=0;l<3;l++)
+                {
+                    t[i][j][l]=-1;
+                }
+            }
+        }
+        return dp(nums, 0,n,k,0);
+    }
+}
