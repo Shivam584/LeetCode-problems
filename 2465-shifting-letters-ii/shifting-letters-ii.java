e660e4cd7a52a67677a2f27cbd0d6bd53aa24611
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-08-10 08:23:36 +0530

    Time: 451 ms (5.07%) | Memory: 128 MB (5.19%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        int sum=0;
+        int t=0,i=0;
+        TreeMap<Integer,Integer> mp= new TreeMap<>();
+        for(int []sh : shifts)
+        {
+            t=(sh[2]==0) ? -1 : 1;
+            mp.put(sh[0],mp.getOrDefault(sh[0],0)+t);
+            mp.put(sh[1]+1,mp.getOrDefault(sh[1]+1,0)-t);
+        }
+        char ch[]=s.toCharArray();
+
+        for(int k : mp.keySet())
+        {
+            System.out.println(i+" "+k+" "+sum);
+            if(i<k)
+            {
+                for(int j=i;j<k;j++)
+                {
+                    int val=sum%26;
+                    int id=ch[j]-'a';
+                    int d=(val<0)? 26: 0;
+                        ch[j]=(char)('a'+(d+val+id)%26);
+                }
+            }
+            sum+=mp.get(k);
+            i=k;
+        }
+
+        return new String(ch);
+    }
+}
