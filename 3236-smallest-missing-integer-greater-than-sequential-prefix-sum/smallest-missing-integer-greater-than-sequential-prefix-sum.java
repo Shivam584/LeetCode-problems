4dd3051a10ebab4f4a220bb25007527dbba2bbe8
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-08-12 02:14:22 +0530

    Time: 1 ms (93.68%) | Memory: 44.1 MB (64.01%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -3,24 +3,19 @@ class Solution {
         Set<Integer> ms=new HashSet<>();
         for(int num: nums)
             ms.add(num);
-        int val=nums[0],mval=nums[0];
-        int c=1,mc=1;
+        int val=nums[0];
+        int c=1;
         for(int i=1;i<nums.length;i++)
         {
             if(val+1==nums[i])
-                {c++;val++;}
+                {
+                    c++;val++;
+                }
             else
-            {
                break;
-            }
-            if(mc<c)
-            {
-                mval=val-c;
-                mc=c;
-            }
         }
-        System.out.println(mval +" "+mc);
-        int sum= mval*mc + (mc*(mc+1))/2;
+        val-=c;
+        int sum= val*c + (c*(c+1))/2;
         while(ms.contains(sum))
             sum++;
         return sum;
