13c47e23c56492f0346f23199f94a77f9460bd6a
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-08-11 17:55:55 +0530

    Time: 48 ms (8.45%) | Memory: 74.3 MB (5.30%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.java
+++ 931-maximum-frequency-stack/maximum-frequency-stack.java
@@ -1,10 +1,10 @@
 class FreqStack {
 
     Map<Integer,Integer> mp;
-    TreeMap<Integer,List<Integer>> mf;
+    TreeMap<Integer,Deque<Integer>> mf;
     public FreqStack() {
         mp= new HashMap<Integer,Integer>();
-        mf= new TreeMap<Integer,List<Integer>>((a,b) -> {
+        mf= new TreeMap<Integer,Deque<Integer>>((a,b) -> {
             return Integer.compare(b,a);
         });
     }
@@ -12,21 +12,21 @@ class FreqStack {
     public void push(int val) {
         mp.put(val,mp.getOrDefault(val,0)+1);
         int c=mp.get(val);
-        List<Integer> ls=mf.get(c);
+        Deque<Integer> ls=mf.get(c);
         if(ls==null)
-            ls=new ArrayList<Integer>();
-        ls.add(val);
+            ls=new ArrayDeque<Integer>();
+        ls.addLast(val);
         mf.put(c,ls);    
     }
     
     public int pop() {
-        Map.Entry<Integer,List<Integer>> entry= mf.firstEntry();
+        Map.Entry<Integer,Deque<Integer>> entry= mf.firstEntry();
         int c=entry.getValue().size();
-       int val= entry.getValue().get(c-1);
+       int val= entry.getValue().peekLast();
        if(c==1)
             mf.remove(mp.get(val));
         else
-            mf.get(mp.get(val)).remove(c-1);
+            mf.get(mp.get(val)).pollLast();
 
         if(mp.get(val)>1)
             mp.put(val,mp.get(val)-1);
