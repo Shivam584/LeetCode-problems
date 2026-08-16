4623953f642966b2314b1ebbcbbb00000b8f0d47
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-08-17 02:33:49 +0530

    Time: 7 ms (4.17%) | Memory: 47.2 MB (5.13%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ class Pair
+{
+    TreeNode node;
+    int b;
+     Pair(TreeNode node,int b)
+    {
+        this.node=node;
+        this.b=b;
+    }
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(obj==null || obj.getClass()!=getClass())
+            return false;
+        
+        Pair p = (Pair)obj;
+        return p.node==this.node && this.b==p.b;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(node,b);
+    }
+}
+class Solution {
+        Map<Pair,Integer> mp;
+        int dp(TreeNode root, int fg)
+        {
+            if(root==null)
+                return 0;
+            Pair p = new Pair(root,fg);
+            if(mp.containsKey(p))
+                return mp.get(p);
+            int res= dp(root.left,0)+ dp(root.right,0);
+            if(fg==0)
+                res=Math.max(root.val +dp(root.left,1)+dp(root.right,1),res);
+            mp.put(p,res);
+            return res;
+        }
+    public int rob(TreeNode root) {
+        mp=new HashMap<Pair,Integer>();
+        return dp(root,0);
+    }
+}
