a2c41800195814570305401bbc2dd2b8874930b0
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-08-27 09:09:06 +0530

    Time: 218 ms (60.53%) | Memory: 44.2 MB (77.41%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -2,11 +2,11 @@ class Pair
 {
     int a,b;
     double c;
-    public Pair(int a,int b)
+    public Pair(int a,int b,double d)
     {
         this.a=a;
         this.b=b;
-        this.c= (double)a/b;
+        this.c= d;
     }
 
     @Override
@@ -15,29 +15,36 @@ class Pair
         if(obj==null || obj.getClass()!=getClass())
             return false;
         Pair p= (Pair)obj;
-        return p==this || (p.a==this.a && p.b==this.b);
+        return p==this || (p.a==this.a && p.b==this.b && p.c==this.c);
     } 
 
     @Override
     public int hashCode()
     {
-        return Objects .hash(a,b);
+        return Objects .hash(a,b,c);
     }
 }
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
         PriorityQueue<Pair> pq= new PriorityQueue<>((A,B)->{
-            return Double.compare(B.c,A.c);
-        });
-
-
-        for(int i=0;i<arr.length-1;i++)
-        for(int j=i+1;j<arr.length;j++)
-        {
-            pq.offer(new Pair(arr[i],arr[j]));
-            if(pq.size()>k)
-                pq.poll();
+            return Double.compare(A.c,B.c);
+        });     
+        int j=arr.length-1,n=arr.length;
+        for(int i=0;i<n-1;i++){
+            pq.offer(new Pair(i,j,(double)arr[i]/arr[j]));
         }
-        return new int[]{pq.peek().a, pq.peek().b};
+
+        while(k>1)
+        {
+            Pair p=pq.poll();
+            k--;
+            if(p.a<p.b)
+            {
+                p.b--;
+                p.c=(double)arr[p.a]/arr[p.b];
+                pq.offer(p);
+            }
+        }
+        return new int[]{arr[pq.peek().a], arr[pq.peek().b]};
     }
 }
