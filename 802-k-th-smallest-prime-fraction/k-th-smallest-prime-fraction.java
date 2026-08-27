e27714fc3d9af5f5b59d9f6789aa80409cff6123
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-08-27 14:10:22 +0530

    Time: 62 ms (72.59%) | Memory: 44 MB (82.89%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -7,31 +7,32 @@ class Solution {
     int n=arr.length;
        while(mx - mn > 1e-12)
        {
-         double mid= mn+(mx-mn)/2;
-        int c=0,p=0,q=1;
-         for(int i=0;i<n-1;i++)
-         {
-            int j=i+1;
-            while(j<n && 1.0*arr[i]>arr[j]*mid)
-                j++;
-            c+=(n-j);
-         
-                if (j < n &&
-                    (double) arr[i] / arr[j] >
-                    (double) p / q) {
+            double mid= mn +(mx-mn)/2.0;
+            int c=0;
+              int v=0,g=1;
+            for(int i=0;i<n-1;i++)
+            {
+                int j=i+1;
+                while(j<n && (arr[i]*0.1) > mid*arr[j])
+                    j++;
+                c+=n-j;
 
-                    p = arr[i];
-                    q = arr[j];
+                if(j<n && (1.0*arr[i])/arr[j] > ((1.0*v)/g))
+                {
+                    v=arr[i];
+                    g=arr[j];
                 }
             }
 
-            if (c < k) {
-                mn = mid;
-            } else {
-                mx = mid;
-                ans[0] = p;
-                ans[1] = q;
-            }
+        if(c<k)
+            mn=mid;
+        else 
+        {
+            mx=mid;
+            ans[0]=v;
+            ans[1]=g;
+        }
+
        }
 
         return ans;
