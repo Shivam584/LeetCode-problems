b5617a23dabf395c9441d25f5ce9f6121b7c006e
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-04 02:55:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (8.55%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,42 +1,28 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n=digits.length;
-            digits[n-1]++;
-        if(digits[n-1]<10)
-            return digits;
-        int newDigits[] = new int[n+1];
+        digits[n-1]++;
+        if(digits[n-1]==10)
+        {
         int q=0,rem=0;
-        int i=n-1,l=0;
+        int i=n-1;
+
         do{
         digits[i]+=q;
-        rem=digits[i]%10;
         q=digits[i]/10;
-        newDigits[l]=rem;
-        l++;
+        digits[i]%=10;
         i--;
         }while(i>-1 && q!=0);
         
         if(q!=0)
-            {newDigits[l]=q;l++;}
-
-        while(i>-1)
-        {
-          newDigits[l]=digits[i];
-          l++;
-          i--;   
+            {
+                int newDigits[] = new int[n+1];
+                newDigits[0]=q;
+                for(int j=0;j<n;j++)
+                      newDigits[j+1]=digits[j];
+                    digits=newDigits;
+            }
         }
-        if(l==n)
-        {
-            for(int j=0;j<n;j++)
-                digits[j]= newDigits[n-j-1];
-            return digits;
-        }
-         for(int j=0;j<newDigits.length/2;j++)
-         {
-            int temp= newDigits[j];
-            newDigits[j]= newDigits[n-j];
-            newDigits[n-j]=temp;
-         }
-        return newDigits;
+        return digits;
     }
 }
