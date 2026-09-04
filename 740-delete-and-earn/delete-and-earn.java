ddfbf88420f6376e02a55b95b2871783ccfc81f1
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-04 18:56:24 +0530

    Time: 15 ms (8.48%) | Memory: 46.3 MB (89.69%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.java
@@ -0,0 +1,29 @@
+class Solution {
+    int t[];
+    int dp(int n, int keys[],  Map<Integer,Integer> mp )
+    {
+        if(n<0)
+            return 0;
+        if(t[n]!=-1)
+            return t[n];
+        int taken =keys[n]*mp.get(keys[n]);
+        if(n-1>-1 && keys[n-1]+1==keys[n])
+            taken+=dp(n-2,keys,mp);
+        else
+            taken+=dp(n-1,keys,mp);
+        
+        return t[n]=Math.max(taken , dp(n-1,keys,mp));
+    }
+    public int deleteAndEarn(int[] nums) {
+        Map<Integer,Integer> mp = new HashMap<>();
+        for(int num : nums)
+            mp.put(num, mp.getOrDefault(num,0)+1);
+        int[] keysArray = mp.keySet().stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(keysArray);
+        int n=keysArray.length;
+        t= new int[n+1];
+        for(int i=0;i<=n;i++)
+        t[i]=-1; 
+        return dp(keysArray.length-1, keysArray,mp);
+    }
+}
