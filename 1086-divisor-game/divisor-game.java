717c819a181262c4e0e912528b423b17e7f480b0
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-04 13:55:28 +0530

    Time: 4 ms (2.43%) | Memory: 42.2 MB (33.84%) - LeetSync

--- 1086-divisor-game/divisor-game.java
+++ 1086-divisor-game/divisor-game.java
@@ -2,13 +2,17 @@ class Solution {
 
     public boolean divisorGame(int n) {
         
-        int dp[]= new int[n+1];
+        boolean dp[]= new boolean[n+1];
         
         for(int i=2;i<=n;i++)
         {
             for(int j=1;j<=i/2;j++)
-                    dp[i]=Math.max(dp[i],dp[i-j]+1);
+                if(i%j==0)
+                {
+                    if(dp[i-j]==false)
+                        {dp[i]=true;break;}
+                }
         }
-        return dp[n]%2==1;
+        return dp[n];
     }
 }
