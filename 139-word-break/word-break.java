97402f3748bfe7a4961619f98a9a9909c739e207
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-04 18:09:34 +0530

    Time: 6 ms (66.29%) | Memory: 46.2 MB (47.07%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,35 +1,40 @@
 class Solution {
     Set<String> hs;
-    int t[][];
-    boolean dp(char ch[], int i,int j)
+    int t[];
+    boolean dp(char ch[], int n)
     {
-        if(i>j)
-            return false;
-        if(hs.contains(new String(ch, i, j-i+1)))
+        if(n<=0)
             return true;
-        
-        if(t[i][j]!=-1)
-            return t[i][j]==1;
+       
+        if(t[n]!=-1)
+            return t[n]==1;
+
         boolean temp=false;
-        for(int k=i;k<j;k++)
+        int k=1;   
+        while(k<=n)
         {
-            temp=temp || (dp(ch,i,k) && dp(ch,k+1,j));
+            if(hs.contains(new String(ch,k-1,n-k+1)))
+                {
+                    if(dp(ch,k-1))
+                          {temp=true;break;}
+                }
+            k++;
         }
-        t[i][j]=(temp) ? 1 : 0;
+      
+        t[n]=(temp) ? 1 : 0;
         return temp;
     }
     public boolean wordBreak(String s, List<String> wordDict) {
          hs = new HashSet<>();
          int n=s.length();
-         t=new int[n+1][n+1];
+         t=new int[n+1];
+
         for(String word : wordDict)
             hs.add(word);
         
-        for(int i=0;i<=n;i++)
-        {
             for(int j=0;j<=n;j++)
-            t[i][j]=-1;
-        }
-        return dp(s.toCharArray(),0,n-1);
+            t[j]=-1;
+    
+        return dp(s.toCharArray(),n);
     }
 }
