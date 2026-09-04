ba7a713989b44f906acffebbab6849b46a6c3664
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-04 11:43:01 +0530

    Time: 1 ms (100.00%) | Memory: 75.9 MB (66.32%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,40 +1,19 @@
 class Solution {
-    int t[][][];
-    int dp(int[] nums, int i,int n, int k, int h)
-    {
-        if((k==0 || i==n) && h==0)
-            return 0;
-        if((k==0 || i==n) && h==1)
-            return Integer.MIN_VALUE;
-        
-        if(t[i][k][h]!=-1)
-            return t[i][k][h];
-
-    int ans= dp(nums,i+1,n,k,h);
-        if(h==0)
-        {
-            ans=Math.max(-nums[i]+dp(nums,i,n,k,1),ans);
-        }
-        else
-        {
-            ans=Math.max(nums[i]+dp(nums,i,n,k-1,0),ans);
-        }
-        return t[i][k][h]=ans;
-    }
+    
     public int maxProfit(int[] nums) {
         int n=nums.length;
-        t=new int[n+1][3][3];
+        int b1=Integer.MIN_VALUE;
+        int b2=Integer.MIN_VALUE;
+        int s1=Integer.MIN_VALUE;
+        int s2=Integer.MIN_VALUE;
         
-        for(int i=0;i<=n;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<3;j++)
-            {
-                for(int k=0;k<3;k++)
-                {
-                    t[i][j][k]=-1;
-                }
-            }
+            b1=Math.max(b1,-nums[i]);
+            s1=Math.max(s1,nums[i]+b1);
+            b2=Math.max(b2,s1-nums[i]);
+            s2=Math.max(s2,nums[i]+b2);
         }
-        return dp(nums, 0,n,2, 0);
+        return s2;
     }
 }
