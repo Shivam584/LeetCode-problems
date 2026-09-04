f9bbbaa4ab6be63f9a4bb37e6769f1bf69bbe8bb
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-05 03:19:02 +0530

    Time: 6 ms (98.68%) | Memory: 71.1 MB (28.00%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,25 +1,38 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int m=matrix.length, n=matrix[0].length;
-        int ans[]= new int[n+1];
-        int previousDiagonal=0;
-        int res=0,temp=0;
-       for(int i=1;i<=m;i++)
-       {
-            for(int j=1;j<=n;j++)
+        int m=matrix.length,n=matrix[0].length;
+        int t[][]= new int[m][n];
+        for(int i=0;i<m;i++)
+            t[i][0]=matrix[i][0]-'0';
+        for(int i=0;i<n;i++)
+            t[0][i]=matrix[0][i]-'0';
+
+        // if(m==1 || n==1)
+        // {
+        //     int ans=0;
+        //      for(int i=0;i<m;i++)
+        //         ans=Math.max(ans,t[i][0]);
+        //     for(int i=0;i<n;i++)
+        //         ans=Math.max(ans,t[0][i]);
+        //     return ans;
+        // }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
             {
-                temp=ans[j];
-                if(matrix[i-1][j-1]=='1')
+                
+                if(matrix[i][j]=='1')
                 {
-                    ans[j]=1+Math.min(Math.min(ans[j],ans[j-1]),previousDiagonal);
-                    res=Math.max(res,ans[j]);
+                    t[i][j]=1+Math.min(t[i-1][j-1],Math.min(t[i][j-1],t[i-1][j]));
                 }
                 else
-                    ans[j]=0;
-                previousDiagonal=temp;
+                    t[i][j]=0;
             }
-       }
-    
-       return res*res;
+        }
+        int ans=0;
+           for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                ans=Math.max(ans,t[i][j]);
+        return ans*ans;
     }
 }
