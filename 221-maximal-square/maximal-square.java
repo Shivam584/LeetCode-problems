e7e5866e2f40958095246c22551d6db8d894db5a
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-05 03:19:33 +0530

    Time: 6 ms (98.68%) | Memory: 70.9 MB (58.46%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -6,28 +6,11 @@ class Solution {
             t[i][0]=matrix[i][0]-'0';
         for(int i=0;i<n;i++)
             t[0][i]=matrix[0][i]-'0';
-
-        // if(m==1 || n==1)
-        // {
-        //     int ans=0;
-        //      for(int i=0;i<m;i++)
-        //         ans=Math.max(ans,t[i][0]);
-        //     for(int i=0;i<n;i++)
-        //         ans=Math.max(ans,t[0][i]);
-        //     return ans;
-        // }
         for(int i=1;i<m;i++)
         {
             for(int j=1;j<n;j++)
-            {
-                
                 if(matrix[i][j]=='1')
-                {
                     t[i][j]=1+Math.min(t[i-1][j-1],Math.min(t[i][j-1],t[i-1][j]));
-                }
-                else
-                    t[i][j]=0;
-            }
         }
         int ans=0;
            for(int i=0;i<m;i++)
