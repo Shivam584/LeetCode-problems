12314364a04574a46e950a5a9dc34d266cb64848
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-06 19:41:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (47.62%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,11 +1,17 @@
 class Solution {
-    
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m+1][n+1];
-        for(int i=1;i<=m;i++)
-        for(int j=1;j<=n;j++)
-        dp[i][j]=(i==1 || j==1) ? 1: dp[i-1][j] + dp[i][j-1];
-
-        return dp[m][n];
+       int arr[][]= new int[m][n];
+       for(int i=0;i<m;i++)
+            arr[i][0]=1;
+        for(int i=0;i<n;i++)
+            arr[0][i]=1;
+       for(int i=1;i<m;i++)
+       {
+        for(int j=1;j<n;j++)
+        {
+            arr[i][j]=arr[i-1][j]+arr[i][j-1];
+        }
+       } 
+       return arr[m-1][n-1];
     }
 }
