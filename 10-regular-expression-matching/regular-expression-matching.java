3b19fa18084db39fa9f30d310deca604066a5e49
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-08 18:55:23 +0530

    Time: 2 ms (46.24%) | Memory: 43.8 MB (14.65%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,51 +1,44 @@
 class Solution {
-     int t[][];
-    boolean dp(int i, int j,String s,String p)
-    {
-        if(i==0 && j==0)
-            return true;
-        if(j==0)
-            return false;
 
-        if(t[i][j]!=-1)
-            return t[i][j]==1;
-        
-        boolean ans=false;
-        if(p.charAt(j-1)=='.')
-        {
-            if(i==0)
-                return false;
-            ans=ans|| dp(i-1,j-1,s,p);
-        }
-        else if(p.charAt(j-1)=='*')
-        {
-            int k=0;
-            ans= ans || dp(i,j-2,s,p);
-            while(k<i && (p.charAt(j-2)=='.' || s.charAt(i-k-1)==p.charAt(j-2)))
-                {ans=ans|| dp(i-k-1,j-2,s,p);k++;}
-        }
-        else 
-        {
-            if(i==0)
-                return false;
-            if(s.charAt(i-1)==p.charAt(j-1))
-                ans = ans || dp(i-1,j-1,s,p);
-        }
-        
-
-        t[i][j] = ans ? 1: 0;
-
-        return ans;
-    }
     public boolean isMatch(String s, String p) {
         int s1=s.length(), p1=p.length();
-        t= new int[s1+1][p1+1];
+        boolean t[][]= new boolean[s1+1][p1+1];
+
         for(int i=0;i<=s1;i++)
         {
             for(int j=0;j<=p1;j++)
-            t[i][j]=-1;
+            {
+                if(i==0 && j==0)
+                     t[i][j]= true;
+                else if(j==0)
+                     t[i][j]= false;
+                else if(p.charAt(j-1)=='.')
+                {
+                    if(i==0)
+                         t[i][j]= false;
+                    else
+                     t[i][j]= t[i][j] || t[i-1][j-1];
+                }
+                else if(p.charAt(j-1)=='*')
+                {
+                    int k=0;
+                     t[i][j] = t[i][j] || t[i][j-2];
+                    while(k<i && (p.charAt(j-2)=='.' || s.charAt(i-k-1)==p.charAt(j-2)))
+                        {
+                            t[i][j]= t[i][j] || t[i-k-1][j-2];
+                            k++;
+                        }
+                }
+                else 
+                {
+                     if(i==0)
+                         t[i][j]= false;
+                    else if(s.charAt(i-1)==p.charAt(j-1))
+                         t[i][j]= t[i][j] || t[i-1][j-1];
+                }
+            }
         }
-        return dp(s1,p1,s,p);
+        return t[s1][p1];
     }
 }
 
