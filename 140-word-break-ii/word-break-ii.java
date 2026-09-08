fef8a00561305c0f7c522184f7a2b23f038c03d4
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-08 18:25:45 +0530

    Time: 3 ms (57.04%) | Memory: 48.7 MB (28.61%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,36 +1,39 @@
 class Solution {
-    StringBuilder s0;
-    void dp(char ch[], int i,int n,List<String> ls, Set<String> hs)
+    int maxLen;
+    Map<Integer,List<String>> mp;
+    List<String> dp(char ch[], int i,int n, Set<String> hs)
     {
         if(i>=n)
-            {
-                if(s0.isEmpty())
-                    return;
-                else
-                ls.add(s0.toString());
-            }
-        for(int j=i;j<n;j++)
+                return List.of("");
+
+        if(mp.containsKey(i))
+            return mp.get(i);
+        
+        List<String> ans = new ArrayList<String>();
+        for(int j=1;j<=maxLen && (i+j)<=n ; j++)
         {
-            String s1= new String(ch,i, j-i+1);
-            int len=j-i+1;
+            String s1= new String(ch,i, j);
             if(hs.contains(s1))
             {        
-                s0.append(s1);
-                if(j!=n-1)
-                    {s0.append(' ');len++;}
-                dp(ch,j+1,n,ls,hs);
-                s0.delete(s0.length() -len, s0.length());
+                List<String> sufixs= dp(ch,i+j,n,hs);   
+                for(String s : sufixs)
+                {
+                    if(s.isEmpty())
+                        ans.add(s1);
+                    else
+                        ans.add(s1+" "+s);
+                }
             }
         }
+        mp.put(i,ans);
+        return ans;
     }
     public List<String> wordBreak(String s, List<String> wordDict) {
-        s0= new StringBuilder();
         char ch[]=s.toCharArray();
+        mp= new HashMap<Integer,List<String>>();
         Set<String> hs = new HashSet<>();
         for(String w : wordDict)
-            hs.add(w);
-        List<String> ls= new ArrayList<>();
-        dp(ch,0,s.length(),ls,hs);
-        return ls;
+            {hs.add(w);maxLen= Math.max(maxLen,w.length());}
+        return dp(ch,0,s.length(),hs);
     }
 }
