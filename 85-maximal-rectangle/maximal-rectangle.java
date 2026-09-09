a2aee449b80e143bdc0f8fd025525c875b675fe3
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-09 18:28:14 +0530

    Time: 18 ms (77.80%) | Memory: 49.6 MB (74.86%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,41 +1,42 @@
 class Solution {
-    int t[][];
-    int resolve(int[][] mat, int m,int n)
+    int cal(int h[], int n)
     {
-        if(m==0 || n==0)
-        return 0;
-        if(m*n==mat[m][n])
-        return mat[m][n];
-        if(t[m][n]!=-1)
-        return t[m][n];
-        int temp=0;
-        for(int i=m;i<mat.length;i++)
+        Stack<Integer> s= new Stack<>();
+        int ans=0;
+        for(int i=0;i<n;i++)
         {
-            for(int j=n;j<mat[0].length;j++)
+            while(!s.isEmpty() && h[s.peek()]>h[i])
             {
-                temp=mat[i][j]-mat[i-m][j]-mat[i][j-n]+mat[i-m][j-n];
-                if(temp==m*n)
-                    break;
+                int k=s.pop();
+                int len = s.isEmpty() ? i : i-s.peek()-1;
+                ans=Math.max(ans,len*h[k]);
             }
-            if(m*n==temp)
-            break;
-        }   
-        if(m*n!=temp)
-            temp=Math.max(resolve(mat,m-1,n-1),Math.max(resolve(mat,m,n-1),resolve(mat,m-1,n)));
-        t[m][n]=temp;
-        return temp;
+            s.push(i);
+        }
+
+            while(!s.isEmpty())
+            {
+                int k=s.pop();
+                int len = s.isEmpty() ? n : n-s.peek()-1;
+                ans=Math.max(ans,len*h[k]);
+            }
+        return ans;
     }
     public int maximalRectangle(char[][] matrix) {
-        int a,b;
-        int m=matrix.length, n=matrix[0].length;
-        int dp[][]= new int[m+1][n+1];
-        t= new int[m+1][n+1];
-        for(int i=1;i<=m;i++)
-            for(int j=1;j<=n;j++)
-               {dp[i][j]=(j>1?dp[i][j-1]:0)+matrix[i-1][j-1]-'0';t[i][j]=-1;}
-        for(int i=1;i<=n;i++)
-            for(int j=2;j<=m;j++)
-               dp[j][i]+=dp[j-1][i];
-        return resolve(dp,m,n);
+        int m=matrix.length,n=matrix[0].length;
+        int t[][]= new int[m][n];
+
+        for(int j=0;j<n;j++)
+            t[0][j]=matrix[0][j]-'0';
+        
+        for(int i=1;i<m;i++)
+        for(int j=0;j<n;j++)
+            if(matrix[i][j]=='1')
+                t[i][j]=1+t[i-1][j];
+        int ans=0;
+
+        for(int i=0;i<m;i++)
+            ans=Math.max(ans,cal(t[i],n));
+        return ans;
     }
 }
