d71fbd75ddcec38be23365e859b88e6aa687beec
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-10 04:19:07 +0530

    Time: 32 ms (20.84%) | Memory: 43.3 MB (66.02%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
@@ -1,38 +1,36 @@
 class Solution {
-    int t[][][];
-    int dp(int i,int j,int []jd,int d)
+    int t[][];
+    int dp(int i,int n,int []jd,int d)
     {
-         if(j-i+1<d)
+         if(n-i<d)
             return 1000_000;   
-        if(t[i][j][d]!=-1)
-            return t[i][j][d];
+        if(t[i][d]!=-1)
+            return t[i][d];
         if(d==1)
         {
             int ans=0;
-            while(i<=j)
+            while(i<n)
                 ans=Math.max(ans,jd[i++]);
                 return ans;
         }
       
         int temp=0,ans=1000_000;
-        int l=d/2;
-        for(int k=i;k<j;k++)
+        int mx=jd[i];
+        for(int k=i;k<n;k++)
         {
-            t[i][k][l]=dp(i,k,jd,l);
-            t[k+1][j][d-l]=dp(k+1,j,jd,d-l);
-            temp =  t[i][k][l] + t[k+1][j][d-l];
+            mx=Math.max(mx,jd[k]);
+            temp = dp(k+1,n,jd,d-1)+mx;
             ans=Math.min(ans,temp);
         }
-        return t[i][j][d]=ans;
+        return t[i][d]=ans;
     }
     public int minDifficulty(int[] jd, int d) {
         int n=jd.length;
-        t= new int[n+1][n+1][d+1];
-        for(int i=0;i<n;i++)
-        for(int j=0;j<n;j++)
+        t= new int[n+1][d+1];
+        for(int i=0;i<=n;i++)
         for(int k=0;k<=d;k++)
-        t[i][j][k]=-1;
-        int ans=dp(0,n-1,jd,d);
+        t[i][k]=-1;
+        int ans=dp(0,n,jd,d);
         return (1000_000>ans) ? ans : -1;
     }
 }
