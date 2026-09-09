4e89d74df4dcc432e022d7e806a6aadd69848b4d
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-10 01:16:39 +0530

    Time: 6 ms (8.50%) | Memory: 47.1 MB (70.50%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,22 +1,26 @@
 class Solution {
-    
+    int t[][];
+    int dp(int i,int j,char w1[],char w2[])
+    {
+        if(i==0 & j==0)
+            return 0;
+        if(j==0)
+            return i;
+        if(i==0)
+            return j;
+        if(t[i][j]!=-1)
+            return t[i][j];
+        int cost=1+dp(i-1,j-1,w1,w2);
+        if(w1[i-1]==w2[j-1])
+            cost--;
+        return t[i][j]=Math.min(cost,1+Math.min(cost,Math.min(dp(i-1,j,w1,w2),dp(i,j-1,w1,w2))));
+    }
     public int minDistance(String word1, String word2) {
-        int m=word1.length(),n=word2.length();
-        int t[][]= new int[m+1][n+1];
-        for(int i=0;i<=m;i++)
-        for(int j=0;j<=n;j++)
-            {
-                if(i==0)
-                    t[i][j]=j;
-                else if(j==0)
-                    t[i][j]=i;
-                else
-                {
-                    t[i][j]=t[i-1][j-1];
-                    if(word1.charAt(i-1) != word2.charAt(j-1))
-                    t[i][j]=1+Math.min(t[i][j],Math.min(t[i][j-1],t[i-1][j]));
-                }
-            }
-        return t[m][n];
+        int w1=word1.length(),w2=word2.length();
+        t= new int[w1+1][w2+1];
+        for(int i=0;i<=w1;i++)
+            for(int j=0;j<=w2;j++)
+                t[i][j]=-1;
+        return dp(w1,w2,word1.toCharArray(),word2.toCharArray());
     }
 }
