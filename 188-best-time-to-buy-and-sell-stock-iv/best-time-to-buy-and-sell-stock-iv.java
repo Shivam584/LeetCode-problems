9a649856a2e102160a766df0fa144e026921810c
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-11 03:06:39 +0530

    Time: 11 ms (9.66%) | Memory: 47.9 MB (9.53%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,25 +1,25 @@
 class Solution {
+    int t[][][];
+    int dp(int i,int n, int p[],int k,boolean hold)
+    {
+        if(k==0)
+            return 0;
+        if(i==n-1)
+            return (hold) ? p[n-1] : 0;
+        int l=hold? 1: 0;
+        if(t[i][k][l]!=-1)
+            return t[i][k][l];
+        if(hold)
+            return t[i][k][1]=Math.max(p[i]+dp(i+1,n,p,k-1,false),dp(i+1,n,p,k,hold));
+        
+        return t[i][k][0]=Math.max(-p[i]+dp(i+1,n,p,k,true),dp(i+1,n,p,k,hold));
+    }
     public int maxProfit(int k, int[] prices) {
-        int b[]= new int[k];
-        int s[]= new int[k];
-        
-        for(int i=0;i<k;i++)
-        {
-            b[i]=Integer.MIN_VALUE;
-            s[i]=Integer.MIN_VALUE;
-        }
-        
-        for(int price : prices)
-        {
-            int temp=0;
-            for(int j=0;j<k;j++)
-            {
-                b[j]=Math.max(b[j],temp-price);
-                s[j]=Math.max(s[j],price+b[j]);
-                temp=s[j];
-            }
-        }
-
-        return s[k-1];
+        int n=prices.length;
+       t= new int [n][k+1][2];
+       for(int i=0;i<n;i++)
+       for(int j=0;j<=k;j++)
+        t[i][j][0]=t[i][j][1]=-1;
+       return dp(0,n,prices,k,false);
     }
 }
