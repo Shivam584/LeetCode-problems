0a6c7328a0b386eaf28b268f1e830beaf7dd913e
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-11 02:27:28 +0530

    Time: 569 ms (5.01%) | Memory: 87 MB (42.87%) - LeetSync

--- 403-frog-jump/frog-jump.java
+++ 403-frog-jump/frog-jump.java
@@ -1,37 +1,27 @@
 class Solution {
-    int t[][];
-    boolean dp(int i,int p,int n, int s[], Map<Integer,Integer> mp)
-    {
-        if(i==n-1)
-            return true;
-        
-        boolean ans=false;
-
-        if(t[i][p]!=-1)
-            return t[i][p]==1;
-        int diff=s[i]-s[p]-1;
-            for(int l=0;l<3;l++)
-            {
-                Integer x= mp.get(s[i]+diff+l);
-                if(x!=null && x!=i)
-                    ans=ans || dp(x,i,n,s,mp);
-            }
-        t[i][p]= ans? 1: 0;
-        return ans;
-    }
-    public boolean canCross(int[] stones) {
-        int n=stones.length;
-        if((stones[n-1]>stones[0] + (((n-1)*n)/2)) || (stones[1]-stones[0]!=1))
+ 
+    public boolean canCross(int[] s) {
+        int n=s.length;
+        if((s[n-1]>s[0] + (((n-1)*n)/2)) || (s[1]-s[0]!=1))
             return false;
-        Map<Integer,Integer> s = new HashMap<>();
+        Map<Integer,Integer> mp = new HashMap<>();
         for(int i=0;i<n;i++)
-            s.put(stones[i],i);
+            mp.put(s[i],i);
 
-        t= new int [n+1][n+1];
-
-        for(int i=0;i<=n;i++)
-            for(int j=0;j<=n;j++)
-                t[i][j]=-1;
-        return dp(1,0,stones.length,stones,s);
+        boolean t[][]= new boolean [n][n];
+        for(int j=0;j<n;j++)
+            t[n-1][j]=true;
+        for(int i=n-1;i>-1;i--)
+            for(int p=i-1;p>=0;p--)
+               {
+                    int diff=s[i]-s[p]-1;
+                    for(int l=0;l<3;l++)
+                    {
+                        Integer x= mp.get(s[i]+diff+l);
+                        if(x!=null && x!=i)
+                            t[i][p]=t[i][p] || t[x][i];
+                    }
+               }
+        return t[1][0];
     }
 }
