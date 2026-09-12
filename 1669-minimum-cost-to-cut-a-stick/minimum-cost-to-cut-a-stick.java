d78c7459bc2fe74280fd216034455b5798a6522a
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-13 05:21:45 +0530

    Time: 36 ms (7.80%) | Memory: 44.6 MB (40.14%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,49 +1,39 @@
-class Pair
-{
-    int a,b;
-    public Pair(int a,int b)
-    {
-        this.a=a;
-        this.b=b;
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        if(obj==null || obj.getClass()!=getClass())
-            return false;
-        Pair p = (Pair)obj;
-        return p==this || (p.a==this.a && p.b==this.b);
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(a,b);
-    }
-}
 
 class Solution {
-    Map<Pair,Integer> mp;
+    int mp[][];
     int dp(int i,int j,int []cuts)
     {
         int fg=0;
         int ans= Integer.MAX_VALUE;
-        Pair p= new Pair(i,j);
-        if(mp.containsKey(p))
-            return mp.get(p);
-        for(int x : cuts)
-            if(i<x && x<j)
+
+        if(mp[i][j]!=-1)
+            return mp[i][j];
+        
+        for(int k=1;k<cuts.length-1;k++)
+            if(i<k && k<j)
             {
                 fg=1;
-                ans=Math.min(ans,j-i+dp(i,x,cuts)+dp(x,j,cuts));
+                ans=Math.min(ans,cuts[j]-cuts[i]+dp(i,k,cuts)+dp(k,j,cuts));
             }
         ans=fg==0 ? 0: ans;
-        mp.put(p,ans);
-        return  ans;
+       
+        return  mp[i][j]=ans;
     }
     public int minCost(int n, int[] cuts) {
-        mp= new HashMap<Pair,Integer>();
-        return dp(0,n,cuts);
+
+        int m=cuts.length;
+        mp= new int [m+2][m+2];
+
+        for(int i=0;i<=m+1;i++)
+        for(int j=0;j<=m+1;j++) 
+            mp[i][j]=-1;
+        Arrays.sort(cuts);
+
+        int nc[]= new int[m+2];
+        for(int i=0;i<m;i++)
+            nc[i+1]=cuts[i];
+            nc[m+1]=n;
+
+        return dp(0,m+1,nc);
     }
 }
