9957e299daa469f0233dd7f395331598905ee1cb
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-15 04:14:22 +0530

    Time: 4 ms (31.98%) | Memory: 44.1 MB (20.75%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        Arrays.sort(arr1);
+        int n=arr2.length;
+        Map<Integer,Integer> mp = new HashMap<>();
+        for(int x: arr2)
+            mp.put(x,0);
+        int c=0;
+        for(int i=0;i<arr1.length;i++)
+        {
+            Integer val=mp.get(arr1[i]);
+            if(val!=null)
+                mp.put(arr1[i],val+1);
+        }
+        int l=arr1.length-1;
+        for(int i=arr1.length-1;i>-1;i--)
+        {
+            if(!mp.containsKey(arr1[i]))
+            {
+                arr1[l]=arr1[i];
+                l--;
+            }
+        }
+        l=0;
+        for(int i=0;i<n;i++)
+        {
+            int c1=mp.get(arr2[i]);
+            for(int j=0;j<c1;j++)
+                arr1[l++]=arr2[i];
+        }
+        return arr1;
+    }
+}
