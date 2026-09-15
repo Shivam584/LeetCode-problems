687c0d6e728a975cfaf46f8fef284bb11a646c00
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-16 04:33:21 +0530

    Time: 1227 ms (7.32%) | Memory: 284.2 MB (63.42%) - LeetSync

---
+++ 3313-maximum-strength-of-k-disjoint-subarrays/maximum-strength-of-k-disjoint-subarrays.java
@@ -0,0 +1,29 @@
+class Solution {
+    long t[][][];
+     long dp( int i,int n, int k, int fg, int nums[])
+     {
+        
+        if(k==0)
+            return 0;
+
+        if(k>n || i==n)
+         return -1000_000_000_000_000l;
+        if(t[i][k][fg]!=-1l)
+            return t[i][k][fg];
+        t[i][k][fg]= ((k%2==1) ? (long)k*nums[i]: -k*((long)nums[i])) + Math.max(dp(i+1,n,k,1,nums),
+        dp(i+1,n,k-1,1,nums));
+
+        if(fg==0)
+            t[i][k][fg]=Math.max(dp(i+1,n,k,0,nums),t[i][k][fg]);
+
+        return t[i][k][fg];
+     }
+    public long maximumStrength(int[] nums, int k) {
+        int n=nums.length;
+        t= new long [n][k+1][2];
+        for(int i=0;i<n;i++)
+            for(int j=0;j<=k;j++)
+                t[i][j][0]=t[i][j][1]=-1l;
+        return dp(0,n,k,0,nums);
+    }
+}
