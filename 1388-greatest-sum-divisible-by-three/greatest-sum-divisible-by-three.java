4d154b8fc4d2d7d3f632a5c23bb40144cd3f0129
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-18 02:10:16 +0530

    Time: 3 ms (97.79%) | Memory: 49.3 MB (87.83%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
@@ -5,51 +5,27 @@ class Solution {
         int sum=0;
         for(int num : nums)
             sum+=num;
-       
         int rem=sum%3;
         if(rem==0)
-            return sum;
-        if(rem==1)
-        {
-             int d21=sum,d22=sum,d1=sum;
-            for(int i=0;i<n;i++)
-            {
-                if(nums[i]%3==1)
-                    d1=Math.min(d1,nums[i]);
+            return sum;    
+        int d11=sum,d12=sum,d21=sum,d22=sum;
 
-                if(nums[i]%3==2)
-                {
-                    if(d21>=nums[i])
-                    {   
-                        d22=d21;
-                        d21=nums[i];
-                    }
-                    else 
-                        d22=Math.min(d22,nums[i]);
-                }
-            }
-          
-            return sum-Math.min(d21+d22,d1);
-        }
-
-        int d11=sum,d12=sum,d2=sum;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]%3==1)
-            {
-                if(d11>=nums[i])
-                {   
-                    d12=d11;
-                    d11=nums[i];
-                }
-                // else 
-                //         d12=Math.min(d12,nums[i]);
-            }
+            if(nums[i]%3==rem)
+                d11=Math.min(d11,nums[i]);
                 
-           if(nums[i]%3==2)
-                d2=Math.min(d2,nums[i]); 
+           if(nums[i]%3==(3-rem))
+            {
+                if(d21>=nums[i])
+                {   
+                    d22=d21;
+                    d21=nums[i];
+                }
+                else 
+                    d22=Math.min(d22,nums[i]);
+            }
         }
-  System.out.println(d11+" "+d12);
-        return sum-Math.min(d11+d12,d2);
+        return sum- Math.min(d11,d21+d22);
     }
 }
