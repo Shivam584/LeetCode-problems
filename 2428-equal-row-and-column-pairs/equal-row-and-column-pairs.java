3986e6ed56593e73d0be769339e18792970671cb
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-19 12:35:13 +0530

    Time: 7 ms (99.14%) | Memory: 48.8 MB (99.90%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,21 +1,35 @@
 class Solution {
+        int getKey(int arr[],int n)
+        {
+            long p=1l;
+            long mod=1_000_000_007l;
+            long k=0;
+            for(int i=0;i<n;i++)
+            {
+                k=(p*arr[i]+k)%mod;
+                p=(p*31)%mod;
+            }
+        return (int)k;
+        }
     public int equalPairs(int[][] grid) {
         int n=grid[0].length;
         int ans=0;
+        Map<Integer,Integer> mp= new HashMap<>();
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                int fg=1;
-                for(int k=0;k<n;k++)
-                {
-                    if(grid[i][k]!=grid[k][j])
-                    {fg=0;break;}
-                }
-                if(fg==1)
-                    ans++;
-            }
+            int k=getKey(grid[i],n);
+             mp.put(k,mp.getOrDefault(k,0)+1);
         }
+        int temp[]= new int[n];
+        for(int i=0;i<n;i++)
+        {
+            for(int l=0;l<n;l++)
+                temp[l]=grid[l][i];
+            Integer val=mp.get(getKey(temp,n));
+            if(val!=null)
+            ans+=val;
+        }
+
         return ans;
     }
 }
