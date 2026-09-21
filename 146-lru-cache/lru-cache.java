7c2cd9dfce6cdfef36dcbcbed5706a9e455289b6
Shivam Agarwal <74584392+Shivam584@users.noreply.github.com>
2026-09-21 13:41:13 +0530

    Time: 14 ms (92.95%) | Memory: 58.6 MB (78.92%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,99 +1,24 @@
-class Node
-{
-    int key,value;
-    Node left,right;
-    public Node(int key, int val)
-    {
-        this.key=key;
-        this.value=val;
-        left=right=null;
-    }
-    @Override
-    public boolean equals(Object obj)
-    {
-        if(obj==this)
-        return true;
-        if(obj==null || obj.getClass()!=getClass())
-            return false;
-        Node n=(Node)obj;
-        return n.key==this.key && this.value==this.value && this.left==n.left && this.right==n.right; 
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(key,value,left,right);
-    }
-}
 class LRUCache {
-    Map<Integer,Node> mp;
-    Node head,tail; 
     int c;
+    Map<Integer,Integer> mp;
     public LRUCache(int capacity) {
-        mp =new HashMap<Integer,Node>();
-        head=tail=null;
         this.c=capacity;
+        mp=new LinkedHashMap<Integer,Integer>(this.c,0.75f,true);
     }
-    private void removeNode(Node node)
-    {
-        mp.remove(node.key);
-        Node prv=node.left;
-        Node nxt=node.right;
-            node.left=node.right=null;
-        if(prv!=null)
-            prv.right=nxt;
-        if(nxt!=null)
-            nxt.left=prv;
-        
-        if(node==head)
-            head=nxt;
-        
-        if(node==tail)     
-            tail=prv;
-    }
-      private void addNode(Node node)
-    {
-        mp.put(node.key,node);
 
-        if(head==null)
-            head=node;
-
-        node.left=tail;
-        if(tail!=null)
-            tail.right=node;
-        tail=node;
-    }
     public int get(int key) {
-        Node node=mp.get(key);
-       if(node==null)
+        if(!mp.containsKey(key))
             return -1;
-
-        if(node!=tail)
-        {
-            removeNode(node);
-            addNode(node);
-        }
-        return tail.value;
+        return mp.get(key);
     }
     
     public void put(int key, int value) {
-        Node node=mp.get(key);
-        if(node==null)
-        {
-            node=new Node(key,value);
-            if(c==0)
-                removeNode(head);
-            else
-                c--;
-        }
-        else
-            removeNode(node);
-        addNode(node);    
-        node.value=value;
-        
+        mp.put(key,value);
+        if(mp.size()>c) 
+        mp.remove(mp.keySet().iterator().next());
     }
 }
-  
+
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
